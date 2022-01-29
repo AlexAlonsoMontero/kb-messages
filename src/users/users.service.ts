cf42af4c557bd07299fb2de25503646e1387a732
alex <lxalonso@gmail.com>
2022-01-29 11:55:54 +0100

    Usuario anonimo hace un get y añade usuario

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import users from './users.entity';
+import { UserDto } from './user.dto';
 
 @Injectable()
 export class UsersService {
@@ -14,8 +15,20 @@ export class UsersService {
             return await this.users.find()
             
         }
-        createUser(){
-            return "crear usuario";
+        async createUser(newUser: UserDto){
+            try{
+                const addedUser =  this.users.create(newUser);
+                const result = await this.users.save(addedUser);
+                console.log(result)
+                return "ok";
+            }catch(error){
+                console.error(error) 
+                return `No se ha podidoañadir a la base de datos 
+                \n codigo de error: ${error.code}
+                \n detalle: ${error.detail}`
+
+            }
+            
         }
         login(){
             return "Login user";
